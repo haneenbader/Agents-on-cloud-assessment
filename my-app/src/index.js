09a8be5aaa3f7e4a0f1b97a13ce7483b85941595
haneen-bader <haneenalwatan993@gmail.com>
2022-06-15 14:08:39 +0300

    auth

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,8 +5,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_DOMAIN}
-    clientId={process.env.REACT_APP_CLIENT_ID}
+    domain="haneenbader.eu.auth0.com"
+    clientId="UrIjErQi6EczWSx4XDerTyEloH9N9BBl"
     redirectUri={window.location.origin}
   >
     <App />
